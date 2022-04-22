4e65f579dfe95c8187b55829f18b0b5da5aaa0a3
thecodershrey <88538030+thecodershrey@users.noreply.github.com>
2022-04-22 15:48:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas=createCanvas(300,300);
+    canvas.center();
+
+}
+
+function draw()
+{
+
+
+}
+
+function take_snapshot()
+{
+    save("myfilterimage.png")
+}
